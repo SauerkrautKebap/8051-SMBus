8ce46da14f7d35cf25e27e5ae9e0971cc27a99c3
David Specht <DavidSpecht@protonmail.com>
2020-01-20 18:37:30 +0100

    Added settings stuct

---
+++ I2C.h
@@ -0,0 +1,20 @@
+#define TIMER0  0b00;
+#define TIMER1  0b01;
+#define TIMER2H 0b10;
+#define TIMER2L 0b11;
+
+
+typedef struct settings_t {
+    char SMBEnable      : 1;
+    char SMBSlaveInhibit: 1;
+    char SMBExtHold     : 1;
+    char SMBSCLTimeout  : 1;
+    char SMBFreeTimeout : 1;
+    char SMBClockSource : 2;
+} settings;
+
+void I2Csettings(struct *settings_t) {
+    
+
+}  
+
